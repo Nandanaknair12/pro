6c5f9c2e92c037f915bd1ecdd0ae67d0ad48905c
Nandanaknair12 <nandanaknair1224@gmail.com>
2020-02-11 20:01:46 +0530

    update

--- app/src/main/java/com/example/ekathapro/requestloan.java
+++ app/src/main/java/com/example/ekathapro/requestloan.java
@@ -6,15 +6,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class requestloan extends AppCompatActivity {
 
+    EditText date,name,amount;
+    String sd,sn,sa;
     Button b1,b2,b3,b4,b5,b6,b7,b8,b9;
+    Button reqloan;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_requestloan);
+
+        date=(EditText)findViewById(R.id.date);
+        name=(EditText)findViewById(R.id.name);
+        amount=(EditText)findViewById(R.id.amount1);
 
         b1=(Button)findViewById(R.id.memberlist);
         b2=(Button)findViewById(R.id.requestloan);
@@ -25,6 +34,31 @@ public class requestloan extends AppCompatActivity {
         b7=(Button)findViewById(R.id.paymentinfo);
         b8=(Button)findViewById(R.id.complaints);
         b9=(Button)findViewById(R.id.privacy);
+
+        reqloan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+               sd=date.getText().toString();
+               sn=name.getText().toString();
+               sa=amount.getText().toString();
+                if(sd.isEmpty())
+                {
+                    date.setError("enter date");
+                    date.requestFocus();
+                }
+                if(sn.isEmpty())
+                {
+                    name.setError("enter name");
+                    name.requestFocus();
+                }
+                else if(sa.isEmpty())
+                {
+                    amount.setError("enter amount");
+                    amount.requestFocus();
+                }
+            }
+        });
 
         b2.setOnClickListener(new View.OnClickListener() {
             @Override
