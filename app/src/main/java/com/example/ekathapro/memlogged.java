744a176a6ff379e54572758e10a884d8df5c0fa8
Nandanaknair12 <nandanaknair1224@gmail.com>
2020-02-11 13:42:16 +0530

    update

--- app/src/main/java/com/example/ekathapro/memlogged.java
+++ app/src/main/java/com/example/ekathapro/memlogged.java
@@ -1,6 +1,9 @@
 package com.example.ekathapro;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.Intent;
@@ -8,15 +11,56 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class memlogged extends AppCompatActivity {
 
     Button b1,b2,b3,b4,b5,b6,b7,b8,b9;
+    DatabaseReference refee;
+    RecyclerView recyclerView;
+    MemberAdapter adapter;
+    ArrayList<Memb> list;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memlogged);
+
+        recyclerView=(RecyclerView)findViewById(R.id.recycle);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        list=new ArrayList<Memb>();
+
+        refee= FirebaseDatabase.getInstance().getReference().child("Member");
+        refee.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+            {
+                for (DataSnapshot studentDatasnapshot : dataSnapshot.getChildren())
+                {
+                    Memb memb = studentDatasnapshot.getValue(Memb.class);
+                        list.add(memb);
+                }
+                adapter = new MemberAdapter(memlogged.this,list);
+                recyclerView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError)
+            {
+                Toast.makeText(getApplicationContext(),"something wnt wrong",Toast.LENGTH_LONG).show();
+            }
+        });
+
 
         b1=(Button)findViewById(R.id.memberlist);
         b2=(Button)findViewById(R.id.requestloan);
@@ -89,7 +133,7 @@ public class memlogged extends AppCompatActivity {
                 SharedPreferences.Editor editor=(SharedPreferences.Editor)getApplicationContext().getSharedPreferences("login", Context.MODE_PRIVATE);
                 editor.clear();
                 editor.commit();
-                
+
             }
         });
     }
