c515bb351d96db35ece86a4460d8dd231c4c5fa5
Nandanaknair12 <nandanaknair1224@gmail.com>
2020-02-15 09:32:10 +0530

    update

--- app/src/main/java/com/example/ekathapro/unitmembers.java
+++ app/src/main/java/com/example/ekathapro/unitmembers.java
@@ -2,13 +2,33 @@ package com.example.ekathapro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class unitmembers extends AppCompatActivity {
+
+    EditText unitname;
+    Button seemem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_unitmembers);
+
+        unitname=(EditText) findViewById(R.id.cdsunitname);
+        seemem=(Button)findViewById(R.id.seemembers);
+
+        seemem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent=new Intent(getApplicationContext(),cdsseemembers.class);
+                startActivity(intent);
+            }
+        });
     }
 }
